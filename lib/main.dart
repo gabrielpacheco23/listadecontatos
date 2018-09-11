4950623593a7885d1f5302e9aaf959fcfa370e21
gabrielpacheco23 <gabrielpacheco23@live.com>
2018-09-11 17:08:13 -0300

    name change

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Contatos',
+      title: 'Agenda de Contatos',
       home: Home(),
     );
   }
